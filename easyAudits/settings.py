415af17e1443007cc8f530b71ee90fdb9fada332
sethmensah <setmensah@gmail.com>
2021-03-10 15:47:20 +0000

    Additional settings

--- easyAudits/settings.py
+++ easyAudits/settings.py
@@ -133,6 +133,6 @@ django_heroku.settings(locals())
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-#AUTH_USER_MODEL = 'app.CustomUser'
+AUTH_USER_MODEL = 'app.CustomUser'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
